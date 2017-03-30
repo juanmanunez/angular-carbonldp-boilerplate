bc7e2decfdcda6fbf7b2501afa7e094dd63b3284
Alvaro Contreras <alvaro.contreras@base22.com>
2017-03-30 11:37:45 -0600

    Moved boot.ts to src and rename it to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,21 @@
-// There are files that reference this two dependencies and therefore they get included in the bundled file
-// This causes a conflict with angular2-polyfills.js, as that file also declares them
-// To avoid this, angular2-polyfills.js is no longer included in the index.html and zone and reflect are declared here instead
-import "zone.js";
-import "reflect-metadata";
-import "zone.js/dist/long-stack-trace-zone";
-
 import { NgModuleRef, enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-
 import { CARBON_PROTOCOL, CARBON_DOMAIN, CARBON_APP_SLUG, DEBUG } from "app/config";
-import { AppModule } from "./app.module";
-
-import { activeContext, appInjector } from "angular2-carbonldp/boot";
-
-import Carbon from "carbonldp/Carbon";
+import { AppModule } from "./app/app.module";
+import { activeContext, appInjector } from "angular-carbonldp/boot";
+import { Class as Carbon } from "carbonldp/Carbon";
 
 let carbon:Carbon = new Carbon();
 // Here you can configure your carbon context, extend your ObjectSchemas, etc.
 // Example:
-if( CARBON_PROTOCOL.toLowerCase() === "http" ) carbon.setSetting( "http.ssl", false );
 carbon.setSetting( "domain", CARBON_DOMAIN );
-
+if( CARBON_PROTOCOL.toLowerCase() === "http" ) carbon.setSetting( "http.ssl", false );
 if( CARBON_APP_SLUG ) activeContext.initialize( carbon, CARBON_APP_SLUG );
 
 if( ! DEBUG ) enableProdMode();
 
 platformBrowserDynamic().bootstrapModule( AppModule ).then( ( moduleRef:NgModuleRef<AppModule> ) => {
-	// Give angular2-carbonldp access to the main injector of the module
+	// Give angular-carbonldp access to the main injector of the module
 	appInjector( moduleRef.injector );
 } ).catch( ( error ) => {
 	console.error( "Couldn't bootstrap the application" );
