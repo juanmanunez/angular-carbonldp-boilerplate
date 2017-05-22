0fad2932bc230abc717fee5c71505db53ae000cd
Alvaro Contreras <alvaro.contreras@base22.com>
2017-05-22 13:29:05 -0500

    Change Carbon initialization to match the new angular-carbonldp update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import { NgModuleRef, enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { CARBON_PROTOCOL, CARBON_DOMAIN, CARBON_APP_SLUG, DEBUG } from "app/config";
+import { CARBON_PROTOCOL, CARBON_DOMAIN, DEBUG } from "app/config";
 import { AppModule } from "app/app.module";
-import { activeContext, appInjector } from "angular-carbonldp/boot";
+import { carbonProvider, appInjector } from "angular-carbonldp/boot";
 import { Class as Carbon } from "carbonldp/Carbon";
 
-let carbon:Carbon = new Carbon();
-// Here you can configure your carbon context, extend your ObjectSchemas, etc.
-// Example:
-carbon.setSetting( "domain", CARBON_DOMAIN );
-if( CARBON_PROTOCOL.toLowerCase() === "http" ) carbon.setSetting( "http.ssl", false );
+let hasSSL:boolean = CARBON_PROTOCOL.toLowerCase() === "https";
+let carbon:Carbon = new Carbon( CARBON_DOMAIN, hasSSL );
 
-if( CARBON_APP_SLUG ) activeContext.initialize( carbon, CARBON_APP_SLUG );
+carbonProvider.initialize( <any>carbon );
 
 if( ! DEBUG ) enableProdMode();
 
