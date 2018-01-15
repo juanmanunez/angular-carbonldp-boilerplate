a6f919ea0efc4faba9166960a6a33c98630764a9
Alvaro Contreras <alvaro.contreras@base22.com>
2018-01-15 11:50:53 -0600

    Remove app slug config variable from config.ts file

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,7 @@
-export const CARBON_PROTOCOL:string = process.env.carbon.protocol;
-export const CARBON_DOMAIN:string = process.env.carbon.domain;
+export const CARBON_PROTOCOL:string = process.env.carbon[ "protocol" ];
+export const CARBON_DOMAIN:string = process.env.carbon[ "domain" ];
 export const DEBUG:boolean = process.env.ENV !== "production";
-export const CARBON_APP_SLUG:string = process.env.carbon.app.slug;
 
-export function BASE_URL(): string {
+export function BASE_URL():string {
 	return process.env.baseUrl
 }
