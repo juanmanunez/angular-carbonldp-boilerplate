515a7302fa3521934ec74e3a54135caf6d6aba25
Alvaro Contreras <alvaro.contreras@base22.com>
2018-01-15 11:54:29 -0600

    Remove routes pointing to secured component that were deleted

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,46 +1,15 @@
 import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { AuthenticatedGuard, NotAuthenticatedGuard } from "angular-carbonldp/guards";
-import { ActiveContextResolver } from "angular-carbonldp/resolvers";
-
 import { HomeView } from "app/home/home.view";
-import { LoginView } from "app/login/login.view";
-import { SecuredView } from "app/secured/secured.view"
-import { ErrorView } from "app/errors/error/error.view";
 import { NotFoundErrorView } from "app/errors/not-found-error/not-found-error.view";
 
 const appRoutes:Routes = [
-	{ path: "", redirectTo: "/home", pathMatch: "full" },
+	{path: "", redirectTo: "/home", pathMatch: "full"},
 	{
 		path: "home",
 		component: HomeView,
-		resolve: {
-			activeContext: ActiveContextResolver
-		},
-		data: {
-			onError: [ "/error" ],
-		}
 	},
-	{
-		path: "login",
-		component: LoginView,
-		canActivate: [ NotAuthenticatedGuard ],
-		data: {
-			onReject: [ "/secured" ],
-			onError: [ "/error" ],
-		}
-	},
-	{
-		path: "secured",
-		component: SecuredView,
-		canActivate: [ AuthenticatedGuard ],
-		data: {
-			onReject: [ "/login" ],
-			onError: [ "/error" ],
-		}
-	},
-	{ path: "error", component: ErrorView },
 	{
 		path: "**",
 		component: NotFoundErrorView,
@@ -48,13 +17,6 @@ const appRoutes:Routes = [
 			title: "404 | Boilerplate",
 		}
 	}
-];
-
-
-export const appRoutingProviders:any[] = [
-	ActiveContextResolver,
-	AuthenticatedGuard,
-	NotAuthenticatedGuard,
 ];
 
 export const routing:ModuleWithProviders = RouterModule.forRoot( appRoutes );
